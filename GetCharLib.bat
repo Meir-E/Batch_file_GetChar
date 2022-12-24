ba92745ff486d23ddf409bb2ef679eb364c02c7e
Meir-E <111319241+Meir-E@users.noreply.github.com>
2022-12-24 20:17:13 +0200

    test

--- GetCharLib.bat
+++ GetCharLib.bat
@@ -7,20 +7,23 @@
 :: Run once , can run inly in batch file , Global
 ::--------------------------------------------------
 :: get
-@ECHO off & TITLE Mei-R
-
-:Main
-::update "result" 
-call GetCharLib.bat :GetChar result
-echo %result%
-call :GetUserControll
-
-goto :Main
-goto :eof
+@ECHO off
+::set tempchar=
+call %*
+goto :EOF
 :: -----------------------------------------------------------------
 :: Functions here
 :: -----------------------------------------------------------------
-:GetUserControll
-	call GetCharLib.bat :GetChar result
-	IF %result%==S (echo yes)
+:Main
+	EXIT /B 0
+:GetChar
+	setlocal
+	:: 0123456789abcdefghijklmnopqrstuvwxyz , can change here what char are available,Mei-R
+	for /F "tokens=1 delims=," %%a in ('"choice /N /C 0123456789abcdefghijklmnopqrstuvwxyz"') do (set "result=%%a")
+	endlocal & set result=%result% & set %~1=%result%
+	EXIT /B 0
+:ConsolePrint <str>
+	setlocal
+	<nul set /p "=%~1"
+	endlocal
 	EXIT /B 0
